0a39b630168e8c76db23d680924ef58b6617447a
Andrew Haoyu Song <song833@eceprog5.ecn.purdue.edu>
2025-04-01 22:15:43 -0400

    one more function

--- linked_list.c
+++ linked_list.c
@@ -38,14 +38,28 @@ void insert_at_tail(struct linked_list *list, size_t value) {
 }
 
 size_t remove_from_head(struct linked_list *list) { 
-  return 0; 
+  struct list_node * q = list->head;
+  if (q == NULL){
+    return NULL;
+  }
+  if (q->value==list->head->value){
+    list->head = list->head->next;
+    free (q);
+    return list->head;
+  }
 }
 
 size_t remove_from_tail(struct linked_list *list) { 
-  return 0; 
+  return 0;
 }
 
-void free_list(struct linked_list list) {}
+void free_list(struct linked_list list) {
+  while (list.head != NULL){
+    struct list_node * p = list.head->next;
+    free (list.head);
+    list.head = p;
+  }
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
