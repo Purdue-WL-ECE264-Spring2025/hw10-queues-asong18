b3a3f5c6271657b754c0ab6a58379abcbc726a22
Andrew Haoyu Song <song833@eceprog5.ecn.purdue.edu>
2025-04-01 22:08:30 -0400

    did some stuff

--- linked_list.c
+++ linked_list.c
@@ -2,15 +2,48 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+/*
+  struct list_node {
+  size_t value;
+  struct list_node *next;
+  };
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+  struct linked_list {
+  struct list_node *head;
+  };
+*/
+struct list_node *new_node(size_t value) { 
+  struct list_node *nd = malloc(sizeof(struct list_node));
+  nd -> value = value;
+  nd -> next = NULL;
+  return nd; 
+}
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) {
+  struct list_node * ptr = new_node (value);
+  ptr -> next = list;
+  return ptr;
+}
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+void insert_at_tail(struct linked_list *list, size_t value) {
+  if (list == NULL) {
+    return new_node (value);
+  }
+  struct list_node * p = list;
+  while ((p->next) != NULL) {
+    p = p -> next;
+  }
+  p -> next = new_node (value);
+  return list;
+}
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+size_t remove_from_head(struct linked_list *list) { 
+  return 0; 
+}
+
+size_t remove_from_tail(struct linked_list *list) { 
+  return 0; 
+}
 
 void free_list(struct linked_list list) {}
 
