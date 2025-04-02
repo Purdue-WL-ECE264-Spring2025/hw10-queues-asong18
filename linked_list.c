8a811249ef74535fbf44b76406612e42701b39d8
Andrew Haoyu Song <song833@eceprog5.ecn.purdue.edu>
2025-04-01 22:20:59 -0400

    don't think anything was right

--- linked_list.c
+++ linked_list.c
@@ -13,40 +13,19 @@
   };
 */
 struct list_node *new_node(size_t value) { 
-  struct list_node *nd = malloc(sizeof(struct list_node));
-  nd -> value = value;
-  nd -> next = NULL;
-  return nd; 
+  return 0;
 }
 
 void insert_at_head(struct linked_list *list, size_t value) {
-  struct list_node * ptr = new_node (value);
-  ptr -> next = list;
-  return ptr;
+  return 0;
 }
 
 void insert_at_tail(struct linked_list *list, size_t value) {
-  if (list == NULL) {
-    return new_node (value);
-  }
-  struct list_node * p = list;
-  while ((p->next) != NULL) {
-    p = p -> next;
-  }
-  p -> next = new_node (value);
-  return list;
+  return 0;
 }
 
 size_t remove_from_head(struct linked_list *list) { 
-  struct list_node * q = list->head;
-  if (q == NULL){
-    return NULL;
-  }
-  if (q->value==list->head->value){
-    list->head = list->head->next;
-    free (q);
-    return list->head;
-  }
+  return 0;
 }
 
 size_t remove_from_tail(struct linked_list *list) { 
@@ -54,11 +33,7 @@ size_t remove_from_tail(struct linked_list *list) {
 }
 
 void free_list(struct linked_list list) {
-  while (list.head != NULL){
-    struct list_node * p = list.head->next;
-    free (list.head);
-    list.head = p;
-  }
+  return 0;
 }
 
 // Utility function to help you debugging, do not modify
